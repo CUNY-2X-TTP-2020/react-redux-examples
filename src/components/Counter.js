d06d02dbd5f63146635610e127f5e46c214efe6c
RickyC0626 <rickychon99@gmail.com>
2020-06-09 12:41:23 -0400

    Simple react counter without redux

---
+++ src/components/Counter.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class Counter extends Component
+{
+    state = { value: 0 };
+    
+    increment = () =>
+    {
+        this.setState(prevState =>
+        ({
+            value: prevState.value + 1
+        }));
+    };
+
+    decrement = () =>
+    {
+        this.setState(prevState =>
+        ({
+            value: prevState.value - 1
+        }));
+    };
+
+    render()
+    {
+        return (
+            <div>
+                {this.state.value}
+                <br/>
+                <button onClick={this.increment}>+</button>
+                <button onClick={this.decrement}>-</button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
