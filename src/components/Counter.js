1425ea60c6de25f2d09db593a5cf91bbd2381c78
RickyC0626 <rickychon99@gmail.com>
2020-06-09 12:47:16 -0400

    Decouple reducer from component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,23 +1,32 @@
 import React, { Component } from 'react';
 
+const counter = (state = { value: 0 }, action) =>
+{
+    switch(action.type)
+    {
+        case "INCREMENT": return { value: state.value + 1 };
+        case "DECREMENT": return { value: state.value - 1 };
+        default: return state;
+    }
+}
+
 class Counter extends Component
 {
-    state = { value: 0 };
+    state = counter(undefined, {});
     
+    dispatch(action)
+    {
+        this.setState(prevState => counter(prevState, action));
+    }
+
     increment = () =>
     {
-        this.setState(prevState =>
-        ({
-            value: prevState.value + 1
-        }));
+        this.dispatch({ type: "INCREMENT" });
     };
 
     decrement = () =>
     {
-        this.setState(prevState =>
-        ({
-            value: prevState.value - 1
-        }));
+        this.dispatch({ type: "DECREMENT" });
     };
 
     render()
