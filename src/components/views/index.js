3deff00acd7e12c9f7527d6cc5d59f330b899c10
RickyC0626 <rickychon99@gmail.com>
2020-06-09 20:14:54 -0400

    Add barrel files for components

---
+++ src/components/views/index.js
@@ -0,0 +1,6 @@
+/**
+ * Barrel file for presentation components, which will be used to 
+ * display the main JSX elements. These will be used in their corresponding 
+ * smart containers.
+ */
+export * from "../views";
